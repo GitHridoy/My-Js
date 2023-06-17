2a8581afa2b4c976e23f3bb5346573fb42e7ced0
gitridoy <mdhridoy10000000@gmail.com>
2023-06-17 08:26:28 +0600

    ls

--- extra.js
+++ extra.js
@@ -1,10 +1,14 @@
-let Names = 'Din gelo: tomar o phtho cahia: sohena jatona tomaro asay bosia puri ami ami agune';
 
-// let Spliting = Names.split(' '); এটার কাজ স্ট্রিং কে এরে তে রুপান্তর করা
-// let Spliting = Names.split('.'); স্ট্রিং এর ভিতরে যতগুলো ডট থাকবে ততবার এরে হিসাবে আলাদা করে দিবে
-// let Spliting = Names.split(''); এটার কাজ হল স্টিং এর ভিতরের প্রত্তেকটা কেরেকটার কে আলাদা করে এরে বানান
-// let Sliceing = Spliting.slice(1, 7); এটার কাজ হল নিরদিস্ট অংসকে কেটে ফেলা
-// let Spliting = Names.split(':'); স্ট্রিং এর ভিতরে যতগুলো ডট থাকবে ততবার এরে হিসাবে আলাদা করে দিবে
 
-console.log(Devide)
+
+
+
+
+let sum = 1;
+
+for(let i = 0; i < 21; i++){
+    sum = sum + i;
+    console.log(i, sum)
+}
+
 
