fbbb49a76162ff2f2d75c1b96b98eb5397f92bd0
gitridoy <mdhridoy10000000@gmail.com>
2023-07-03 07:06:11 +0600

    fibo

--- extra.js
+++ extra.js
@@ -1,12 +1,16 @@
 
 
-let first = 10;
-let second = 1;
+// const fibo = [0, 1];
 
-let temp = first;
-first = second;
-second = temp;
+// for(let is = 2; is <= 10; is++){
+//     fibo[is] = fibo[is-1] + fibo[is-2];
+    
+// }
+// console.log(fibo)
 
 
 
-console.log(first, second)
+function add(a, b){
+    return a + b;
+  }
+  console.log(add("adam" + "eve"))
