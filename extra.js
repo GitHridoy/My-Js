f1f34377a709872e06910ed16e2d257f8ce790d6
GitHridoy <mdhridoy10000000@gmail.com>
2023-07-12 20:08:22 +0600

    chair

--- extra.js
+++ extra.js
@@ -1,8 +1,31 @@
 
 
-var model = ['symphony', 'walton', 'lenevo', 'tinmo', 'winmax'];
+// for(let is = 1; is <= 50; is++){
+//      if(is % 3 === 0 && is % 5 === 0){
+//           console.log('foo')
+//      }
+//      else if(is % 3 === 0){
+//           console.log('foobar')
+//      }
+//      else if(is % 5 === 0){
+//           console.log('bar')
+//      }
+//      else{
+//           console.log(is)
+//      }
+// }
 
+function woodCalculator(chairQuantity, tableQuantity, bedQuantity){
+     const perChairWood = 3;
+     const perTableWood = 10;
+     const perBedWood = 50;
 
-var SpliceRemover = model.splice(1, 3);
+     const chairWood = chairQuantity * perChairWood;
+     const tableWood = tableQuantity * perTableWood;
+     const bedWood = bedQuantity * perBedWood;
+     const totalwood = chairWood + tableWood + bedWood;
+     return totalwood;
+}
 
-console.log(model)
+const TotalWood = woodCalculator(0, 0, 1);
+console.log(TotalWood)
